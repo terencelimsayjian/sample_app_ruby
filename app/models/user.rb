3aed48f2e2ed22c822d4080b99c0f8b45f5f8d89
terence lim <terencelim92@gmail.com>
2016-11-07 00:12:47 +0800

    finish modeling-users

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # inside the user model, self is not unnecessary on the right side
+  # before_save { email.downcase! }
+  before_save { self.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
+
+end
